8ce72b6a390a2f0a05c7502406e47d4fe3a8c84c
rosadavi <davi.rosa.ofc@gmail.com>
2026-04-29 14:49:40 -0300

    first commit

---
+++ src/main/java/com/rosadavi/festpay/FestpayApplication.java
@@ -0,0 +1,13 @@
+package com.rosadavi.festpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosadavi/festpay/FestpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosadavi.festpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
