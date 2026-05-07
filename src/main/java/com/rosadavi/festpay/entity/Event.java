751b79180add1351b15c8f0d5b29d1cbdb922e3a
rosadavi <davi.rosa.ofc@gmail.com>
2026-05-07 11:09:09 -0300

    feat: create event entity

---
+++ src/main/java/com/rosadavi/festpay/entity/Event.java
@@ -0,0 +1,47 @@
+package com.rosadavi.festpay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private String location;
+
+    private LocalDate date_initial;
+
+    private LocalDate date_final;
+
+    private LocalDate created_at;
+
+    private LocalDate updated_at;
+
+    @PrePersist
+    public void prePersist() {
+        this.created_at = LocalDate.now();
+        this.updated_at = LocalDate.now();
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_default_id")
+    private UserUpdated userUpdated;
+}
